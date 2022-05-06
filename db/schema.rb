8cdd2919898a010cdbf05021bb5a69ea120f21fa
AbdulRaheel <abdul.raheel@devsinc.com>
2022-05-06 15:12:19 +0500

    Complete working project

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,127 +12,127 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_02_102943) do
-
+ActiveRecord::Schema.define(version: 20_220_502_102_943) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "pg_trgm"
-  enable_extension "pgcrypto"
-  enable_extension "plpgsql"
-  enable_extension "uuid-ossp"
+  enable_extension 'pg_trgm'
+  enable_extension 'pgcrypto'
+  enable_extension 'plpgsql'
+  enable_extension 'uuid-ossp'
 
-  create_table "active_storage_attachments", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
-    t.datetime "created_at", null: false
-    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
-    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  create_table 'active_storage_attachments', force: :cascade do |t|
+    t.string 'name', null: false
+    t.string 'record_type', null: false
+    t.bigint 'record_id', null: false
+    t.bigint 'blob_id', null: false
+    t.datetime 'created_at', null: false
+    t.index ['blob_id'], name: 'index_active_storage_attachments_on_blob_id'
+    t.index %w[record_type record_id name blob_id], name: 'index_active_storage_attachments_uniqueness',
+                                                    unique: true
   end
 
-  create_table "active_storage_blobs", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.bigint "byte_size", null: false
-    t.string "checksum", null: false
-    t.datetime "created_at", null: false
-    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  create_table 'active_storage_blobs', force: :cascade do |t|
+    t.string 'key', null: false
+    t.string 'filename', null: false
+    t.string 'content_type'
+    t.text 'metadata'
+    t.bigint 'byte_size', null: false
+    t.string 'checksum', null: false
+    t.datetime 'created_at', null: false
+    t.index ['key'], name: 'index_active_storage_blobs_on_key', unique: true
   end
 
-  create_table "cart_products", force: :cascade do |t|
-    t.bigint "cart_id", null: false
-    t.bigint "product_id", null: false
-    t.integer "quantity", default: 1
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["cart_id"], name: "index_cart_products_on_cart_id"
-    t.index ["product_id"], name: "index_cart_products_on_product_id"
+  create_table 'cart_products', force: :cascade do |t|
+    t.bigint 'cart_id', null: false
+    t.bigint 'product_id', null: false
+    t.integer 'quantity', default: 1
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['cart_id'], name: 'index_cart_products_on_cart_id'
+    t.index ['product_id'], name: 'index_cart_products_on_product_id'
   end
 
-  create_table "carts", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.decimal "sub_total", default: "0.0"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "coupon_id"
-    t.index ["coupon_id"], name: "index_carts_on_coupon_id"
-    t.index ["user_id"], name: "index_carts_on_user_id"
+  create_table 'carts', force: :cascade do |t|
+    t.bigint 'user_id', null: false
+    t.decimal 'sub_total', default: '0.0'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.bigint 'coupon_id'
+    t.index ['coupon_id'], name: 'index_carts_on_coupon_id'
+    t.index ['user_id'], name: 'index_carts_on_user_id'
   end
 
-  create_table "comments", force: :cascade do |t|
-    t.string "comment_text"
-    t.bigint "product_id", null: false
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["product_id"], name: "index_comments_on_product_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
+  create_table 'comments', force: :cascade do |t|
+    t.string 'comment_text'
+    t.bigint 'product_id', null: false
+    t.bigint 'user_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['product_id'], name: 'index_comments_on_product_id'
+    t.index ['user_id'], name: 'index_comments_on_user_id'
   end
 
-  create_table "coupons", force: :cascade do |t|
-    t.string "code"
-    t.decimal "discount", null: false
-    t.date "expiry_date", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'coupons', force: :cascade do |t|
+    t.string 'code'
+    t.decimal 'discount', null: false
+    t.date 'expiry_date', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "order_details", force: :cascade do |t|
-    t.integer "quantity"
-    t.bigint "order_id"
-    t.bigint "product_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["order_id"], name: "index_order_details_on_order_id"
-    t.index ["product_id"], name: "index_order_details_on_product_id"
+  create_table 'order_details', force: :cascade do |t|
+    t.integer 'quantity'
+    t.bigint 'order_id'
+    t.bigint 'product_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['order_id'], name: 'index_order_details_on_order_id'
+    t.index ['product_id'], name: 'index_order_details_on_product_id'
   end
 
-  create_table "orders", force: :cascade do |t|
-    t.decimal "total"
-    t.bigint "user_id"
-    t.bigint "coupon_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["coupon_id"], name: "index_orders_on_coupon_id"
-    t.index ["user_id"], name: "index_orders_on_user_id"
+  create_table 'orders', force: :cascade do |t|
+    t.decimal 'total'
+    t.bigint 'user_id'
+    t.bigint 'coupon_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['coupon_id'], name: 'index_orders_on_coupon_id'
+    t.index ['user_id'], name: 'index_orders_on_user_id'
   end
 
-  create_table "products", force: :cascade do |t|
-    t.string "descriptio"
-    t.string "price"
-    t.string "float"
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "product_name"
-    t.uuid "uuid", default: -> { "uuid_generate_v4()" }
-    t.integer "quantity"
-    t.index ["user_id"], name: "index_products_on_user_id"
+  create_table 'products', force: :cascade do |t|
+    t.string 'descriptio'
+    t.string 'price'
+    t.string 'float'
+    t.bigint 'user_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.string 'product_name'
+    t.uuid 'uuid', default: -> { 'uuid_generate_v4()' }
+    t.integer 'quantity'
+    t.index ['user_id'], name: 'index_products_on_user_id'
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "full_name"
-    t.string "phone"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  create_table 'users', force: :cascade do |t|
+    t.string 'email', default: '', null: false
+    t.string 'encrypted_password', default: '', null: false
+    t.string 'reset_password_token'
+    t.datetime 'reset_password_sent_at'
+    t.datetime 'remember_created_at'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.string 'full_name'
+    t.string 'phone'
+    t.index ['email'], name: 'index_users_on_email', unique: true
+    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
   end
 
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "carts", "coupons"
-  add_foreign_key "comments", "products"
-  add_foreign_key "comments", "users"
-  add_foreign_key "order_details", "orders"
-  add_foreign_key "order_details", "products"
-  add_foreign_key "orders", "coupons"
-  add_foreign_key "orders", "users"
-  add_foreign_key "products", "users"
+  add_foreign_key 'active_storage_attachments', 'active_storage_blobs', column: 'blob_id'
+  add_foreign_key 'carts', 'coupons'
+  add_foreign_key 'comments', 'products'
+  add_foreign_key 'comments', 'users'
+  add_foreign_key 'order_details', 'orders'
+  add_foreign_key 'order_details', 'products'
+  add_foreign_key 'orders', 'coupons'
+  add_foreign_key 'orders', 'users'
+  add_foreign_key 'products', 'users'
 end
