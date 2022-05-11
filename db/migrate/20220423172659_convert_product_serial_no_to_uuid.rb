14a615b1266d953c9a068ce2ed3f55b1c7adeb34
AbdulRaheel <abdul.raheel@devsinc.com>
2022-05-11 17:17:26 +0500

    added database validations

--- db/migrate/20220423172659_convert_product_serial_no_to_uuid.rb
+++ db/migrate/20220423172659_convert_product_serial_no_to_uuid.rb
@@ -9,7 +9,7 @@ class ConvertProductSerialNoToUuid < ActiveRecord::Migration[5.2]
     change_table :products do |t|
       t.remove :serial_no
     end
-    add_column :products, :uuid, :uuid, default: 'uuid_generate_v4()'
+    add_column :products, :uuid, :uuid, default: 'uuid_generate_v4()', null: false
   end
 
   def def(_down)
--- db/migrate/20220502102919_create_orders_table.rb
+++ db/migrate/20220502102919_create_orders_table.rb
@@ -4,7 +4,7 @@
 class CreateOrdersTable < ActiveRecord::Migration[5.2]
   def change
     create_table :orders do |t|
-      t.decimal :total
+      t.decimal :total, null: false
       t.references :user, foreign_key: true
       t.references :coupon, foreign_key: true
       t.timestamps
